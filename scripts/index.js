cb7a3ff2c35608aed29b3aac80290bcf5e5a60ac
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-20 15:28:22 -0300

    feat: play/pause song timeout 2000ms

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,22 @@
 const bracoVitrola = document.querySelector("#braco-vitrola");
 const disco = document.querySelector("#disco");
+const song = new Audio("songs/The Moon Song.mp3");
 
 
 bracoVitrola.addEventListener("click", () => {
     if (bracoVitrola.classList.contains("animate-braco-tocar")) {
+        song.pause();
         bracoVitrola.classList.add("animate-braco-parar");
         bracoVitrola.classList.remove("animate-braco-tocar");
     } else {
         bracoVitrola.classList.add("animate-braco-tocar");
+        setTimeout(() => {
+            song.play();
+        }, 2000);
         bracoVitrola.classList.remove("animate-braco-parar");
     }
 
-    disco.classList.toggle("animate-spin");
-})
+    setTimeout(() => {
+        disco.classList.toggle("animate-spin");
+    }, 2000);
+});
