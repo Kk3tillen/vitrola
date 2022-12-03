27190d154a68963d9cf15825fb62d82fe5579cc5
Kézia Ketillen <k.kezia@academico.ifrn.edu.br>
2022-12-02 22:56:24 -0300

    feat: vitrola com cartas

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,31 @@
 const bracoVitrola = document.querySelector("#braco-vitrola");
 const disco = document.querySelector("#disco");
-const song = new Audio("songs/The Moon Song.mp3");
 
+const cartasLinks = {
+    "fran": "public/songs/Carta de Francielen.mp4",
+    "kezia": "public/songs/Carta de Kézia.mp4",
+    "paola": "public/songs/Carta de Maria Eduarda Paola.mp4",
+    "duda": "public/songs/Carta de Maria Eduarda Cardoso.mp4"
+}
+let currentAudio = new Audio(cartasLinks["fran"]);
+
+function playCarta(name){
+    currentAudio = new Audio(cartasLinks[name]);
+}
 
 bracoVitrola.addEventListener("click", () => {
+  playVitrola(currentAudio);
+});
+
+function playVitrola(audio){
     if (bracoVitrola.classList.contains("animate-braco-tocar")) {
-        song.pause();
+        audio.pause();
         bracoVitrola.classList.add("animate-braco-parar");
         bracoVitrola.classList.remove("animate-braco-tocar");
     } else {
         bracoVitrola.classList.add("animate-braco-tocar");
         setTimeout(() => {
-            song.play();
+            audio.play();
         }, 2000);
         bracoVitrola.classList.remove("animate-braco-parar");
     }
@@ -19,4 +33,4 @@ bracoVitrola.addEventListener("click", () => {
     setTimeout(() => {
         disco.classList.toggle("animate-spin");
     }, 2000);
-});
+}
