8b5e26d4d4c851265bdafb39a85d1dc8064962ae
Kézia Ketillen <k.kezia@academico.ifrn.edu.br>
2022-12-06 14:05:24 -0300

    feat: add novos audios

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,61 @@
 const bracoVitrola = document.querySelector("#braco-vitrola");
 const disco = document.querySelector("#disco");
+const ANIMATION_BRACO_VITROLA_MS = 1000;
 
 const cartasLinks = {
-    "fran": "public/songs/Carta de Francielen.mp4",
-    "kezia": "public/songs/Carta de Kézia.mp4",
-    "paola": "public/songs/Carta de Maria Eduarda Paola.mp4",
-    "duda": "public/songs/Carta de Maria Eduarda Cardoso.mp4"
+    "fran": "public/songs/CartaFrancielen.m4a",
+    "kezia": "public/songs/CartaKezia.m4a",
+    "paola": "public/songs/CartaEduardaPaola.m4a",
+    "duda": "public/songs/CartaMariaEduardaCardoso.m4a",
+    "clara": "public/songs/CartaClara.m4a",
+    "jerison": "public/songs/CartaJerison.m4a",
+    "fernanda": "public/songs/CartaFernanda.m4a",
+    "evilin": "public/songs/CartaEvilin.m4a",
+    "vinicius": "public/songs/CartaVinicius.m4a",
+    "emilly": "public/songs/CartaEmilly.mp3",
+    "alex": "public/songs/CartaAlex.m4a",
+    "milena": "public/songs/CartaMilene.m4a",
+    "bruno": "public/songs/CartaBruno.m4a"
 }
 let currentAudio = new Audio(cartasLinks["fran"]);
 
 function playCarta(name){
-    currentAudio = new Audio(cartasLinks[name]);
+    if (!currentAudio.paused) {
+        currentAudio.pause();
+        encolherBracoVitrola();
+        setTimeout(() => {
+            currentAudio = new Audio(cartasLinks[name]);
+            playVitrola(currentAudio);
+        }, ANIMATION_BRACO_VITROLA_MS);
+    } else {
+        currentAudio = new Audio(cartasLinks[name]);
+        playVitrola(currentAudio);
+    }
+
 }
 
 bracoVitrola.addEventListener("click", () => {
   playVitrola(currentAudio);
 });
 
+function encolherBracoVitrola() {
+    bracoVitrola.classList.add("animate-braco-parar");
+    bracoVitrola.classList.remove("animate-braco-tocar");
+}
+
 function playVitrola(audio){
     if (bracoVitrola.classList.contains("animate-braco-tocar")) {
         audio.pause();
-        bracoVitrola.classList.add("animate-braco-parar");
-        bracoVitrola.classList.remove("animate-braco-tocar");
+        encolherBracoVitrola();
     } else {
         bracoVitrola.classList.add("animate-braco-tocar");
         setTimeout(() => {
             audio.play();
-        }, 2000);
+        }, ANIMATION_BRACO_VITROLA_MS);
         bracoVitrola.classList.remove("animate-braco-parar");
     }
 
     setTimeout(() => {
         disco.classList.toggle("animate-spin");
-    }, 2000);
+    }, ANIMATION_BRACO_VITROLA_MS);
 }
